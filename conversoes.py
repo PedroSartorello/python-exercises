87efaadd6674a2b66879ad161495e8a32515460a
Pedro H. Sartorello <pedro.sartorello@hotmail.com>
2026-05-19 16:35:30 -0300

    Adicionando as Listas 0 1 e 2

---
+++ conversoes.py
@@ -0,0 +1,12 @@
+# a. celsius_para_fahrenheit(c)
+# b. fahrenheit_para_celsius(f)
+# c. metros_para_centimetros(m)
+
+def celsius_para_fahrenheit(c):
+    return (9/5)*(c + 32)
+
+def fahrenheit_para_celsius(f):
+    return (5/9)*(f - 32)
+
+def metros_para_centimetros(m):
+    return m*100
